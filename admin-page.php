ff0ea899d03f6fa0e8ddcd0c8e5470440422b1a6
ankeris <31132643+ankeris@users.noreply.github.com>
2017-12-01 13:52:41 +0100

    save

--- admin-page.php
+++ admin-page.php
@@ -12,8 +12,9 @@
         
         
         Įžanga:
-        <button type="button" id="addBtn-izanga" @click="addNewArticleForm_exordium">+</button>
-
+            <button type="button" id="addBtn-izanga" @click="addNewArticleForm_exordium">+</button>
+            <br>
+        
             <section class="form-content" style="border: 1px solid black; margin: 5px;" v-for="(data, index) in exordium">
                 {{index}}<h3>{{data.author}}</h3><h4>{{data.title}}</h4>
                 <label>Autorius (vardas, pavardė)</label><br>
@@ -29,36 +30,36 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="izanga">
                 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
-            </section>
+                <button id="exordium" type="button" class="removeBtn" @click="removeForm_exordium(data)">Trinti</button>
+            </section> 
         <br>
 
         
         Istorija:
-        <button type="button" id="addBtn-izanga" @click="addNewArticleForm_history">+</button>
+            <button type="button" id="addBtn-izanga" @click="addNewArticleForm_history">+</button>
+            <br>
+
+            <section class="form-content" style="border: 1px solid black; margin: 5px;" v-for="(data, index) in history">
+
+                    {{index}}<h3>{{data.author}}</h3><h4>{{data.title}}</h4>
+                    <label>Autorius (vardas, pavardė)</label><br>
+                    <input type="text" id="name" name="author_name[]" class="form-input" v-model="data.author">
+                    <br>
+
+                    <label>Straipsnio pavadinimas:</label><br>
+                    <textarea name="article_name[]" class="form-input" v-model="data.title"></textarea>
+                    <br>
+
+                    <label>PDF:</label>
+                    <input type="file" name="pdf_file[]" class="form-input">
+                    <!-- hidden value to identify topic -->
+                    <input type="hidden" name="topic[]" class="form-input" value="istorija">
+
+                    <button id="history" type="button" class="removeBtn" @click="removeForm_history(data)">Trinti</button>
+
+            </section>
         <br>
-           
-        <section class="form-content" style="border: 1px solid black; margin: 5px;" v-for="(data, index) in history">
-
-                {{index}}<h3>{{data.author}}</h3><h4>{{data.title}}</h4>
-                <label>Autorius (vardas, pavardė)</label><br>
-                <input type="text" id="name" name="author_name[]" class="form-input" v-model="data.author">
-                <br>
-
-                <label>Straipsnio pavadinimas:</label><br>
-                <textarea name="article_name[]" class="form-input" v-model="data.title"></textarea>
-                <br>
-
-                <label>PDF:</label>
-                <input type="file" name="pdf_file[]" class="form-input">
-                <!-- hidden value to identify topic -->
-                <input type="hidden" name="topic[]" class="form-input" value="istorija">
-
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
-                
-        </section>
-        <br>
-         
+        
         Politika:
         <button type="button" id="addBtn-izanga" @click="addNewArticleForm_politics">+</button>
         <br>
@@ -80,7 +81,7 @@
                 <input type="hidden" name="topic[]" class="form-input" value="politika">
 
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="politics" type="button" class="removeBtn" @click="removeForm_politics(data)">Trinti</button>
                 
         </section>
         <br>
@@ -105,7 +106,7 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="teise">
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="law" type="button" class="removeBtn" @click="removeForm_law(data)">Trinti</button>
                 
         </section>
         <br>
@@ -130,7 +131,7 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="kalba">
                 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="language" type="button" class="removeBtn" @click="removeForm_language(data)">Trinti</button>
                 
         </section>
         <br>
@@ -155,7 +156,7 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="komunikacija">
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="communication" type="button" class="removeBtn" @click="removeForm_communication(data)">Trinti</button>
                 
         </section>
         <br>
@@ -180,7 +181,7 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="recenzijos">
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="reviews" type="button" class="removeBtn" @click="removeForm_reviews(data)">Trinti</button>
                 
         </section>
         <br>
@@ -206,7 +207,7 @@
                 <input type="hidden" name="topic[]" class="form-input" value="komentarai">
 
                
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="comments" type="button" class="removeBtn" @click="removeForm_comments(data)">Trinti</button>
                 
         </section>
         <br>
@@ -232,7 +233,7 @@
                 <input type="hidden" name="topic[]" class="form-input" value="bibliografija">
 
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="bibliography" type="button" class="removeBtn" @click="removeForm_bibliography(data)">Trinti</button>
                 
         </section>
         <br>
@@ -258,7 +259,7 @@
                 <input type="hidden" name="topic[]" class="form-input" value="kalendorius">
 
 
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="calender" type="button" class="removeBtn" @click="removeForm_calender(data)">Trinti</button>
                 
         </section>
         <br>
@@ -283,16 +284,20 @@
                 <!-- hidden value to identify topic -->
                 <input type="hidden" name="topic[]" class="form-input" value="informacinestechnologijos">
 
-
-
-                <button type="button" class="removeBtn" @click="removeForm(data)">Trinti</button>
+                <button id="it" type="button" class="removeBtn" @click="removeForm_it(data)">Trinti</button>
                 
         </section>
         <br>
         
         <button type="submit">Publikuoti</button>
     </form>
-
+                
+                
+                <section id="preview" v-for="(data, index) in data">
+                    <div>{{exordium.name}}</div>
+                    
+                     
+                </section>
 </div>
 
 
@@ -306,144 +311,167 @@
 const app = new Vue({
     el: "#app",
     data: {
-        exordium: [{
-            author: '',
-            title: '',
-            data: 'izanga'
-        }],
-        history: [{
-            author: '',
-            title: '',
-            data: 'istorija'
-        }],
-        politics: [{
-            author: '',
-            title: '',
-            data: 'politika'
-        }],
-        law: [{
-            author: '',
-            title: '',
-            data: 'teise'
-        }],
-        language: [{
-            author: '',
-            title: '',
-            data: 'kalba'
-        }],
-        communication: [{
-            author: '',
-            title: '',
-            data: 'komunikacija'
-        }],
-        reviews: [{
-            author: '',
-            title: '',
-            data: 'recenzijos'
-        }],
-        comments: [{
-            author: '',
-            title: '',
-            data: 'komentarai'
-        }],
-        bibliography: [{
-            author: '',
-            title: '',
-            data: 'bibliografija'
-        }],
-        calender: [{
-            author: '',
-            title: '',
-            data: 'kalendorius'
-        }],
-        it: [{
-            author: '',
-            title: '',
-            data: 'informacinestechnologijos'
-        }]
+        articles: [{
+            exordium: [
+
+            ],
+            history: [
+
+            ],
+            politics: [
+
+            ],
+            law: [
+
+            ],
+            language: [
+
+            ],
+            communication: [
+
+            ],
+            reviews: [
+
+            ],
+            comments: [
+
+            ],
+            bibliography: [
+
+            ],
+            calender: [
+
+            ],
+            it: [
+
+            ]
+        }
+
+        ]
+
     },
     methods: {
         addNewArticleForm_exordium () {
-            this.exordium.push({
+            this.articles.exordium.push({
                 name: '',
                 title: '',
-                data: 'izanga'
+                show: true
             })
         },
         addNewArticleForm_history () {
-            this.history.push({
+            this.articles.history.push({
                 name: '',
                 title: '',
-                data: 'istorija'
+                show: true
             })
         },
         addNewArticleForm_politics () {
-            this.politics.push({
+            this.articles.politics.push({
                 name: '',
                 title: '',
-                data: 'politika'
+                show: true
             })
         },
         addNewArticleForm_law () {
-            this.law.push({
+            this.articles.law.push({
                 name: '',
                 title: '',
-                data: 'teise'
+                show: true
             })
         },
         addNewArticleForm_language () {
-            this.language.push({
+            this.articles.language.push({
                 name: '',
                 title: '',
-                data: 'kalba'
+                show: true
             })
         },
         addNewArticleForm_communication () {
-            this.communication.push({
+            this.articles.communication.push({
                 name: '',
                 title: '',
-                data: 'komunikacija'
+                show: true
             })
         },
         addNewArticleForm_reviews () {
-            this.reviews.push({
+            this.articles.reviews.push({
                 name: '',
                 title: '',
-                data: 'recenzijos'
+                show: true
             })
         },
         addNewArticleForm_comments () {
-            this.comments.push({
+            this.articles.comments.push({
                 name: '',
                 title: '',
-                data: 'komentarai'
+                show: true
             })
         },
         addNewArticleForm_bibliography () {
-            this.bibliography.push({
+            this.articles.bibliography.push({
                 name: '',
                 title: '',
-                data: 'bibliografija'
+                show: true
             })
         },
         addNewArticleForm_calender () {
-            this.calender.push({
+            this.articles.calender.push({
                 name: '',
                 title: '',
-                data: 'kalendorius'
+                show: true
             })
         },
         addNewArticleForm_it () {
-            this.it.push({
+            this.articles.it.push({
                 name: '',
                 title: '',
-                data: 'informacinestechnologijos'
+                show: true
             })
         },
-        removeForm (data) {
+        removeForm_exordium (data) {
            this.exordium.splice(this.exordium.indexOf(data), 1)
             console.log(this.exordium.indexOf(data));
-        }
+        },
+        removeForm_history (data) {
+           this.history.splice(this.history.indexOf(data), 1)
+            console.log(this.history.indexOf(data));
+        },
+        removeForm_politics (data) {
+           this.politics.splice(this.politics.indexOf(data), 1)
+            console.log(this.politics.indexOf(data));
+        },
+        removeForm_law (data) {
+           this.law.splice(this.law.indexOf(data), 1)
+            console.log(this.law.indexOf(data));
+        },
+        removeForm_language (data) {
+           this.language.splice(this.language.indexOf(data), 1)
+            console.log(this.language.indexOf(data));
+        },
+        removeForm_communication (data) {
+           this.communication.splice(this.communication.indexOf(data), 1)
+            console.log(this.communication.indexOf(data));
+        },
+        removeForm_reviews (data) {
+           this.reviews.splice(this.reviews.indexOf(data), 1)
+            console.log(this.reviews.indexOf(data));
+        },
+        removeForm_comments (data) {
+           this.comments.splice(this.comments.indexOf(data), 1)
+            console.log(this.comments.indexOf(data));
+        },
+        removeForm_bibliography (data) {
+           this.bibliography.splice(this.bibliography.indexOf(data), 1)
+            console.log(this.bibliography.indexOf(data));
+        },
+        removeForm_calender (data) {
+           this.calender.splice(this.calender.indexOf(data), 1)
+            console.log(this.calender.indexOf(data));
+        },
+        removeForm_it (data) {
+           this.it.splice(this.it.indexOf(data), 1)
+            console.log(this.it.indexOf(data));
+        },
     }
 })
 
@@ -451,12 +479,15 @@ const app = new Vue({
 </script>
 
 
-
-
-
-
-
-
+<!--
+        removeForm (event, data) {
+        let targetId = event.currentTarget.id;
+        let target = window[targetId];         
+        console.log(target);
+          
+        this.targetId.splice(this.targetId.indexOf(data), 1);
+       }
+-->
 
 
 
